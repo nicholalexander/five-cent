fcf5b36d0f9835a9e7ebc999a445a866dc1a2b8a
nichol alexander <nichol.alexander@gmail.com>
2017-03-05 21:54:03 -0500

    require the parameters and name them.

--- lib/block.rb
+++ lib/block.rb
@@ -6,7 +6,7 @@ class Block
     'blurgh'
   end
 
-  def initialize(index, previous_hash, timestamp, data, hash)
+  def initialize(index:, previous_hash:, timestamp:, data:, hash:)
     @index = index
     @previous_hash = previous_hash
     @timestamp = timestamp
--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -3,14 +3,19 @@ require 'spec_helper.rb'
 describe "block" do
   it "should be initialized with index, previous_hash, timestamp, data, and hash" do
     data = {data: 'blurgh'}
-    block = Block.new(3, 'asdf', Time.now, data,
-      'asdf2')
+    block = Block.new(index: 3, previous_hash: 'asdf', timestamp: Time.now, data: data,
+      hash: 'asdf2')
     expect(block.index).to eq(3)
     expect(block.previous_hash).to eq('asdf')
     expect(block.timestamp.class).to eq(Time)
     expect(block.data).to eq({data: 'blurgh'})
     expect(block.hash).to eq('asdf2')
   end
+
+  it "should require all params to be initialized" do
+      expect{Block.new(index: 3)}.to raise_error(ArgumentError)
+  end
+
 
   it "should do this blurgh thing" do
     expect(Block.blurgh).to eq("blurgh")
